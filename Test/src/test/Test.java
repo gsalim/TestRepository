0f585b9b53c70104d56e0cec7141276eacf79df7
golam.alim <golam.alim@gmail.com>
2016-05-03 13:04:42 +0600

    Initial Check In

---
+++ Test/src/test/Test.java
@@ -0,0 +1,38 @@
+package test;
+
+public class Test {
+
+	public static String getStringOf(String str, int slot) {
+
+		int count = 0, a = 0, b = str.length();
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '|') {
+				count++;
+				if (count == slot - 1) {
+					a = i + 1;
+				} else if (count == slot) {
+					b = i;
+					break;
+				}
+			}
+		}
+
+		if (slot > count + 1) {
+			return null;
+		}
+		return str.substring(a, b);
+	}
+
+	public Test() {
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "a|bb|ccc|ddd";
+
+		System.out.println(getStringOf(str, 4));
+
+		System.exit(0);
+	}
+}
